e0f40935c4a44a5b8bc7be410609c303d9904be5
Mathis BILLIARD <mathis.billiard@student.junia.com>
2023-01-05 18:24:16 +0100

    Ajoutez des fichiers projet.

---
+++ PUSH_GIT/PUSH_GIT.cpp
@@ -0,0 +1,12 @@
+﻿// PUSH_GIT.cpp : définit le point d'entrée de l'application.
+//
+
+#include "PUSH_GIT.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
